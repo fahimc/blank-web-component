853afd1860fa2844746a27087fce9fbb75b0636b
Fahim Chowdhury <fahim@gmx.com>
2015-09-21 16:09:33 +0100

    working on jet dev release.js

---
+++ grunt/modules/release.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var fs = require('fs');
+
+var TaskRunner = {
+  init: function (grunt) {
+    this.loadNPM(grunt);
+    this.register(grunt);
+    return this.getGruntConfig(grunt);
+  },
+  loadNPM: function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.loadNpmTasks('grunt-shell-spawn');
+  },
+  getGruntConfig: function (grunt) {
+  	var directory = grunt.option('project');
+
+    return {
+    	shell: {
+    		getReleaseBranch: {
+				command: 'git checkout release',
+				options: {
+					execOptions: {
+						cwd: './'+grunt.option('name')+'/'
+					}
+				}
+			}
+    	}
+    };
+  },
+  registerCustomTasks:{
+
+  },
+  register: function (grunt) {
+    grunt.registerTask('release', ['lint']);
+  }
+
+}
+module.exports = TaskRunner.init.bind(TaskRunner);
