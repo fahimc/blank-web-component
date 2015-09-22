deaa19e4af7eefe5578b111c5f146948b0760181
Fahim Chowdhury <fahim.chowdhury@thomsonreuters.com>
2015-09-22 09:55:54 +0100

    adding user prompt

--- grunt/modules/release.js
+++ grunt/modules/release.js
@@ -1,9 +1,13 @@
 var path = require('path');
 var fs = require('fs');
+var prompt = require('prompt');
 
 var TaskRunner = {
+  _replaceBower:false,
+  _grunt:null,
   version: '',
   init: function (grunt) {
+    this._grunt=grunt;
     this.loadNPM(grunt);
     this.register(grunt);
     return this.getGruntConfig(grunt);
@@ -18,6 +22,26 @@ var TaskRunner = {
     var directory = grunt.option('project');
 
     return {
+      replace: {
+        bower: {
+          options: {
+            patterns: [
+            {
+              match: /bower_components/g,
+              replacement: '..'
+            }
+            ]
+          },
+          files: [
+          {
+            cwd: './',
+            expand: true,
+            src: ['**/*.{html,xhtml,htm,js}', '!**/bower_components/**', '!**/node_modules/**', '!**/lib/**', '!**/Gruntfile.js'],
+            dest: './'
+          }
+          ]
+        }
+      },
       shell: {
         getReleaseBranch: {
           command: 'git checkout release',
@@ -25,6 +49,11 @@ var TaskRunner = {
             execOptions: {
               cwd: './'
             }
+          }
+        },
+        mergeMasterBranch: {
+          command: 'git merge master',
+          options: {
           }
         },
         fetchTags: {
@@ -55,12 +84,62 @@ var TaskRunner = {
               done();
             }
           }
+        },
+        createReleaseTag: {
+          command: "git tag -a <%= grunt.option(\"versionNumber\") %> -m \"Release <%= grunt.option(\"versionNumber\") %>\"" ,
+          options: {
+          }
+        },
+        commitReleaseBranch: {
+          command: 'git add --all && git commit -m "Replaced tags"',
+          options: {
+          }
+        },
+        pushReleaseBranch: {
+          command: 'git push origin release --follow-tags',
+          options: {
+          }
         }
       }
     };
   },
   registerCustomTasks: {
-
+    tagPrompt:function(){
+      var done = this.async();
+      console.log('Are you sure you want to create '+TaskRunner._grunt.option("versionNumber")+' tag?');
+      prompt.message ='type \'y\' or \'n\' and hit enter';
+      prompt.start();
+      prompt.get(['continue'], function (err, result) {
+        if(result.continue.trim().toLowerCase() === "y"){
+          console.log("\nokay creating tag "+TaskRunner._grunt.option("versionNumber"));
+          done();
+        }else{
+          TaskRunner._grunt.fail.fatal("tag cancelled");
+        }
+      });
+    },
+    replacePrompt:function(){
+      var done = this.async();
+      console.log('do you want to replace bower file paths?');
+      prompt.message ='type \'y\' or \'n\' and hit enter';
+      prompt.start();
+      prompt.get(['continue'], function (err, result) {
+        if(result.continue.trim().toLowerCase() === "y"){
+          console.log("\nokay replacing bower paths");
+          TaskRunner._replaceBower=true;
+        }else{
+          console.log("\nokay NOT replacing bower paths");
+          TaskRunner._replaceBower=false;
+        }
+        done();
+      });
+    },
+    replaceBower:function(){
+      if(TaskRunner._replaceBower){
+        grunt.task.run( 'replace' );
+      }else{
+      }
+    }
   },
   bumpVersionNumber: function (grunt) {
     var type = "patch";
@@ -72,23 +151,28 @@ var TaskRunner = {
     var arr = grunt.option("versionNumber").split(".");
     switch (type) {
       case 'minor':
-        arr[1] = Number(arr[1]) + 1;
-        arr[2] = "0";
-        break;
+      arr[1] = Number(arr[1]) + 1;
+      arr[2] = "0";
+      break;
       case 'patch':
-        arr[2] = Number(arr[2]) + 1;
-        break;
+      arr[2] = Number(arr[2]) + 1;
+      break;
       case 'major':
-        arr[0] = "v" + String(Number(arr[0].replace("v", "")) + 1);
-        arr[1] = "0";
-        arr[2] = "0";
-        break;
+      arr[0] = "v" + String(Number(arr[0].replace("v", "")) + 1);
+      arr[1] = "0";
+      arr[2] = "0";
+      break;
     }
     console.log("New Tag created" + arr.join("."));
     grunt.option("versionNumber", arr.join("."));
   },
   register: function (grunt) {
-    grunt.registerTask('release', ['shell:getReleaseBranch', 'shell:fetchTags', 'shell:getLatestTag']);
+    //register custom tasks
+    for(var key in this.registerCustomTasks){
+      grunt.registerTask(key,this.registerCustomTasks[key])
+    }
+    //register standard tasks
+    grunt.registerTask('release', ['shell:getReleaseBranch', 'shell:fetchTags','shell:mergeMasterBranch','replacePrompt','replaceBower', 'shell:commitReleaseBranch', 'shell:getLatestTag','tagPrompt','shell:createReleaseTag']);
   }
 
 }
