2820bea2de7766c329428ed3f4da7eb2cde0c0c5
Fahim Chowdhury <fahim@gmx.com>
2015-10-06 14:38:51 +0100

    Merged release version

---
+++ _Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+  });
+
+
+  grunt.registerTask('release', function(){
+    console.log("done release using local grunt file");
+  });
+
+};
